f47b26af6e66fcbe53dccc156b3e6d3115f6dc05
avidrutham <divyamathur89@gmail.com>
2018-10-09 12:23:48 +0530

    added post request to upload files

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+
 
 // require and use "multer"...
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/'});
+
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
+app.get('/hello', (req, res) => {
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+  res.send({"name": req.file.originalname, "type": req.file.mimetype, "size":req.file.size });
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
